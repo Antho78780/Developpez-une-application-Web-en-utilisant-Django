3525a53537535c281e3d71aa0f0b1f228a3f62de
Anthony Audrain <anthonyaud78@gmail.com>
2023-02-06 23:48:57 +0100

    amélioration de la vue

--- litReview/ticket/views.py
+++ litReview/ticket/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def ticket(request):
     return render(request, "ticket.html")
+
+def critique(request):
+    return render(request, "critique.html")
+
+def flux(request):
+    return render(request, "flux.html")
