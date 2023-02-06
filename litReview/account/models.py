4cf04d979cee2d9628a1b324db4aaefe6a7c0f17
Anthony Audrain <anthonyaud78@gmail.com>
2023-02-06 23:50:05 +0100

    amélioration du model de l'application

--- litReview/account/models.py
+++ litReview/account/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class User(models.Model):
-   username = models.CharField(max_length=50)
-   password = models.CharField(max_length=50)
+class User(AbstractUser):
+   pass
+
+
+
+
 
