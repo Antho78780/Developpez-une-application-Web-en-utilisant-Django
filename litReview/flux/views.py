06e3ef57b002d5b4ee320750032585671e835aa3
Anthony Audrain <anthonyaud78@gmail.com>
2023-02-21 14:03:59 +0100

    amélioration de toutes les fonctions

--- litReview/flux/views.py
+++ litReview/flux/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 from .forms import TicketForm, ReviewForm
-from .models import Ticket
+from .models import Ticket, Review
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 
 
-@login_required(login_url="login")
+@login_required
 def flux(request):
     ticket = Ticket.objects.all()
-    return render(request, "flux.html", context={"ticket": ticket})
+    review = Review.objects.all()
+    return render(request, "flux.html", context={"ticket": ticket, "review": review})
 
 
-@login_required(login_url="login")
+@login_required
 def create_ticket(request):
     form = TicketForm()
     if request.method == "POST":
@@ -20,15 +21,36 @@ def create_ticket(request):
             form_save = form.save(commit=False)
             form_save.user = request.user
             form_save.save()
-            return redirect("home")
+            return redirect("flux")
     return render(request, "ticket.html", context={"form": form})
 
-@login_required(login_url="login")
-def create_review(request):
+@login_required
+def create_review_and_ticket(request):
+    formReview = ReviewForm()
+    formTicket = TicketForm()
     if request.method == "POST":
-        print("Formulaire envoyé")
+        formReview = ReviewForm(request.POST)
+        formTicket = TicketForm(request.POST, request.FILES)
 
-    return render(request, "review.html")
+        if all([formReview.is_valid(), formTicket.is_valid()]):
+            formTicket_save = formTicket.save(commit=False)
+            formTicket_save.user = request.user
+            formTicket_save.save()
+            formReview_save = formReview.save(commit=False)
+            formReview_save.user = request.user
+            formReview_save.ticket = formTicket_save
+            formReview_save.save()
+            return redirect("flux")
+
+    return render(request, "review.html", context={"formReview": formReview, "formTicket": formTicket})
+
+@login_required
+def create_review_response(request):
+    formReview = ReviewForm()
+    if request.method == "POST":
+        formReview = ReviewForm(request.POST)
+
+    return render(request, "review_response.html", context={"formReview": formReview})
 
 
 
