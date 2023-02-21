9fbec881f3ab21e8c94d6a32738791ad44033218
Anthony Audrain <anthonyaud78@gmail.com>
2023-02-21 13:58:23 +0100

    ajout du decorateur login_required

--- litReview/subscription/views.py
+++ litReview/subscription/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def subscription(request):
     if request.method == "POST":
-        pass
+        print("recherhce du nom d'utilisateur")
     return render(request, "subscription.html")
