b340be490b6032d7516aae4768520b8c10f0bf35
Anthony Audrain <anthonyaud78@gmail.com>
2023-03-15 14:02:22 +0100

    amélioration du css

--- litReview/account/models.py
+++ litReview/account/models.py
@@ -3,5 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     username = models.CharField(max_length=40, unique=True)
+    followed = models.ManyToManyField("self", through="flux.UserFollows",through_fields=["followed_user", "user",], symmetrical=False)
     
     USERNAME_FIELD = "username"
